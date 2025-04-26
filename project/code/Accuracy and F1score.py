6520204e60986dce56c6d4055072662d5b8e97d1
GM520117 <rose55622@gmail.com>
2025-04-26 16:16:30 +0800

    Add initial project files and setup environment

--- project/code/Accuracy and F1score.py
+++ project/code/Accuracy and F1score.py
@@ -5,3 +5,9 @@ FN = 0    # 因為 Recall = 1
 
 accuracy = TP / (TP + FP + FN)
 print(f"Accuracy ≈ {accuracy:.4f}")  # 應該會是 1.0000 左右
+
+precision = 0.998
+recall = 1.0
+
+f1 = 2 * (precision * recall) / (precision + recall)
+print(f"F1-score = {f1:.4f}")
