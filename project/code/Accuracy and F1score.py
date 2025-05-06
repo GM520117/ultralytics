7e081104ddcb202ad3eb6c82ba3e77d19cf63974
GM520117 <rose55622@gmail.com>
2025-05-06 12:51:26 +0800

    Add initial project files and setup environment

--- project/code/Accuracy and F1score.py
+++ project/code/Accuracy and F1score.py
@@ -1,12 +1,12 @@
-TP = 137  # 全部預測中都是對的（因為 Recall = 1）
+TP = 169  # 全部預測中都是對的（因為 Recall = 1）
 #FP = round((1 - 0.998) * TP / 0.998)  # 四捨五入
-FP = (1 - 0.998) * TP / 0.998
+FP = (1 - 0.999) * TP / 0.999
 FN = 0    # 因為 Recall = 1
 
 accuracy = TP / (TP + FP + FN)
 print(f"Accuracy ≈ {accuracy:.4f}")  # 應該會是 1.0000 左右
 
-precision = 0.998
+precision = 0.999
 recall = 1.0
 
 f1 = 2 * (precision * recall) / (precision + recall)
--- project/code/data.py
+++ project/code/data.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
-model = YOLO(r"C:\Users\user\Downloads\YOLOv8\ultralytics\segment\train1\weights\best.pt")  # 你的模型
-metrics = model.val(data=r"C:\Users\user\Downloads\YOLOv8\ultralytics\Pharmaceutical.v9-721-new.yolov8\data.yaml")  # 輸出驗證結果
+model = YOLO(r"C:\Users\user\Downloads\YOLOv8\ultralytics\segment\train-FCB(100-640)\weights\best.pt")  # 你的模型
+metrics = model.val(data=r"C:\Users\user\Downloads\YOLOv8\ultralytics\Capsule.v9-fcb-two.yolov8\data.yaml")  # 輸出驗證結果
 
 # metrics 會自動包含 precision、recall、F1-score、mAP 等
 print(metrics.box.f1)  # 或 metrics.box.precision, etc.
